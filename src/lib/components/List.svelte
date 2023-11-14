76e1998993e71072c5e539fbedfe814fc45dc928
Tjips <thomas@scheepers.com>
2023-11-14 14:40:14 +0100

    Added tags to test the filter

--- src/lib/components/List.svelte
+++ src/lib/components/List.svelte
@@ -3,13 +3,13 @@
 
 <section>
     <ul id="filterMe">
-        <li>Rood</li>
-        <li>Blauw</li>
-        <li>Groen</li>
-        <li>Geel</li>
-        <li>Grijs</li>
-        <li>Bruin</li>
-        <li>Paars</li>
+        <li id="klimaatadaptatie">Rood</li>
+        <li id="klimaatadaptatie">Blauw</li>
+        <li id="waterkwaliteit">Groen</li>
+        <li id="klimaatadaptatie">Geel</li>
+        <li id="brain">Grijs</li>
+        <li id="klimaatadaptatie">Bruin</li>
+        <li id="waterkwaliteit">Paars</li>
     </ul>
 </section>
 
@@ -24,5 +24,35 @@
         background-color: rgb(239, 239, 239);
         margin-bottom: .5rem;
         border-radius: .5rem;
+    }
+
+    #klimaatadaptatie:after {
+        content: "Klimaatadaptatie";
+        background-color: #ffffff;
+        border: 1px solid #4ECD5D;
+        border-radius: .5rem;
+        margin-left: 1rem;
+        padding: .2rem .5rem;
+        font-size: .7rem;
+    }
+
+    #waterkwaliteit:after {
+        content: "Klimaatadaptatie";
+        background-color: #ffffff;
+        border: 1px solid #4e72cd;
+        border-radius: .5rem;
+        margin-left: 1rem;
+        padding: .2rem .5rem;
+        font-size: .7rem;
+    }
+
+    #brain:after {
+        content: "Klimaatadaptatie";
+        background-color: #ffffff;
+        border: 1px solid #cd614e;
+        border-radius: .5rem;
+        margin-left: 1rem;
+        padding: .2rem .5rem;
+        font-size: .7rem;
     }
 </style>
